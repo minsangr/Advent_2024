4c098a5c9b26e1126374de49757a0dec36cd1743
Minsang Ryu <minsangr@nycstudents.net>
2024-12-10 13:08:40 -0500

    Day 3 part 1 finished

--- Day-03/main.py
+++ Day-03/main.py
@@ -8,16 +8,16 @@ def get_file_data(file_name):
     return data
 
 file_data = get_file_data("Day03Input.txt")
-print(file_data)
 
-regex = "mul\\([1-9]{1,3},[1-9]{1,3}\\)"
-matches = re.findall(regex, file_data[0])
-print(matches)
+regex = "mul\\([0-9]+,[0-9]+\\)"
 
 sum = 0
-for pair in matches:
-    nums = pair[4:int(len(pair) - 1)]
-    num = nums.split(",")
-    sum += (int(num[0]) * int(num[1]))
+
+for i in file_data:
+    matches = re.findall(regex, i)
+    for pair in matches:
+        nums = pair[4:int(len(pair) - 1)]
+        num = nums.split(",")
+        sum += (int(num[0]) * int(num[1]))
 
 print(sum)
