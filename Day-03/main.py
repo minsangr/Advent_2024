168810dae6fff3612d58d430ae96b240baff50dc
Minsang Ryu <minsangr@nycstudents.net>
2024-12-11 12:56:52 -0500

    Day 3 Part 2 done

--- Day-03/main.py
+++ Day-03/main.py
@@ -9,15 +9,23 @@ def get_file_data(file_name):
 
 file_data = get_file_data("Day03Input.txt")
 
-regex = "mul\\([0-9]+,[0-9]+\\)"
+regex = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)"
 
 sum = 0
+do = True
 
 for i in file_data:
     matches = re.findall(regex, i)
+    print(matches)
     for pair in matches:
-        nums = pair[4:int(len(pair) - 1)]
-        num = nums.split(",")
-        sum += (int(num[0]) * int(num[1]))
+        if pair == "do()":
+            do = True
+        elif pair == "don't()":
+            do = False
+        else:
+            if do:
+                nums = pair[4:int(len(pair) - 1)]
+                num = nums.split(",")
+                sum += (int(num[0]) * int(num[1]))
 
 print(sum)
