f501e3bbb1baedd75590182e9552d80e07c075b1
BT_4N2_13 <MINSANGR@NYCSTUDENTS.NET>
2024-12-12 13:21:24 -0500

    DAY 4 PART 1 ALMOST DONE

--- Day-04/main.py
+++ Day-04/main.py
@@ -27,8 +27,54 @@ def horizontalStraight(row):
 def horizontalReverse(row):
     count = 0
     for i in range(3, len(row)):
-        if row[i] == "S" and row[i - 1] == "A" and row[i - 2] == "M" and row[i - 3] == "X":
+        if row[i] == "X" and row[i - 1] == "M" and row[i - 2] == "A" and row[i - 3] == "S":
             count += 1
+    return count
+
+def verticalStraight(col):
+    count = 0
+    for i in range(0, len(col) - 3):
+        if col[i] == "X" and col[i + 1] == "M" and col[i + 2] == "A" and col[i + 3] == "S":
+            count += 1
+    return count
+
+def verticalReverse(col):
+    count = 0
+    for i in range(3, len(col)):
+        if col[i] == "X" and col[i - 1] == "M" and col[i - 2] == "A" and col[i - 3] == "S":
+            count += 1
+    return count
+
+def diagonalStraightRight(grid):
+    count = 0
+    for row in range(0, len(grid) - 3):
+        for col in range(0, len(grid[row]) - 3):
+            if grid[row][col] == "X" and grid[row + 1][col + 1] == "M" and grid[row + 2][col + 2] == "A" and grid[row + 3][col + 3] == "S":
+                count += 1
+    return count
+
+def diagonalReverseLeft(grid):
+    count = 0
+    for row in range(3, len(grid)):
+        for col in range(3, len(grid[row])):
+            if grid[row][col] == "X" and grid[row - 1][col - 1] == "M" and grid[row - 2][col - 2] == "A" and grid[row - 3][col - 3] == "S":
+                count += 1
+    return count
+
+def diagonalStraightLeft(grid):
+    count = 0
+    for row in range(0, len(grid) - 3):
+        for col in range(3, len(grid[row])):
+            if grid[row][col] == "X" and grid[row + 1][col - 1] == "M" and grid[row + 2][col - 2] == "A" and grid[row + 3][col - 3] == "S":
+                count += 1
+    return count
+
+def diagonalReverseLeft(grid):
+    count = 0
+    for row in range(3, len(grid)):
+        for col in range(0, len(grid[row]) - 3):
+            if grid[row][col] == "X" and grid[row - 1][col + 1] == "M" and grid[row - 2][col + 2] == "A" and grid[row - 3][col + 3] == "S":
+                count += 1
     return count
 
 singleRow = get_file_data("singleRowTest.txt")
@@ -38,3 +84,23 @@ print(horizontalStraight(singleRow[0]))
 singleRow2 = get_file_data("singleRowTest2.txt")
 print(singleRow2)
 print(horizontalReverse(singleRow2[0]))
+
+singleCol = get_file_data("singleColTest.txt")
+print(singleCol)
+print(verticalStraight(singleCol))
+
+singleCol2 = get_file_data("singleColTest2.txt")
+print(singleCol2)
+print(verticalReverse(singleCol2))
+
+quadCol = get_file_data("quadColTest.txt")
+print(quadCol)
+print(diagonalStraightRight(quadCol))
+
+quadCol2 = get_file_data("quadColTest2.txt")
+print(quadCol2)
+print(diagonalReverseLeft(quadCol2))
+
+quadCol3 = get_file_data("quadColTest3.txt")
+print(quadCol3)
+print(diagonalStraightLeft(quadCol3))
